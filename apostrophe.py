8560aecb22df57aefe86723ae0f112b23700179b
Alexandre da Silva Barbosa <alexandrebarbosa8371@gmail.com>
2023-02-14 12:00:02 -0300

    apostrophe-exercise.py
    First exercise!

---
+++ apostrophe.py
@@ -0,0 +1,6 @@
+# This code is to demonstrate how to use double and single quotes correctly!
+
+message1 = "One of Python's strengths is its diverse community"
+message2 = 'What does "Python é a melhor linguagem" mean?'
+print(message1)
+print("\n", message2)
